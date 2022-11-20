a764f081bf9d29fd9326d56a39db742c6a0e2c8b
JimboMachine <xbox00210@gmail.com>
2022-11-20 19:41:14 +0800

    login Page doing
    I edit some css syntax to make it prettier. Please help me complete it.

--- Page.js
+++ Page.js
@@ -5,7 +5,7 @@ var attempt = 3; // 可變數來計算嘗試次數。
 function myFormCheck(){// 以下功能單擊登錄按鈕執行。
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    if ( username === "Formget" && password === "formget#123"){
+    if ( username === "0 && password === "00"){
         alert ("Login successfully");
         window.location = "javascript:;"; // 重定向到其他頁面。
         return false;
@@ -14,12 +14,14 @@ function myFormCheck(){// 以下功能單擊登錄按鈕執行。
         attempt --;// 減少一個。
         alert("You have left "+attempt+" attempt;");
 // 3次嘗試後禁用字段。
+/*
         if( attempt === 0){
             document.getElementById("username").disabled = true;
             document.getElementById("password").disabled = true;
             document.getElementById("submit").disabled = true;
             return false;
         }
+        */
     }
 }
 //第種寫法// 記住我 passport remember-me  (NodeJS Express Passport Remember me)
