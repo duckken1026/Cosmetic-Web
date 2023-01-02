c01d626cc2f704f600b87c514ca4fb93d540eabf
YenChieh0716 <t108590037@ntut.org.tw>
2023-01-02 20:07:18 +0800

    Update Login.php

--- Login/Login.php
+++ Login/Login.php
@@ -1,20 +1,22 @@
 <!--login會員登入-->
-    <head>
-      <title>我的帳號: 登入-COSMETIC.COM</title>
-    </head>
-    <body>
-      <div id = "out_block">
-        <div id="fixed_block">
 
-        </div>
-      </div>
+<head>
+  <title>我的帳號: 登入-COSMETIC.COM</title>
+</head>
+
+<body>
+  <div id="out_block">
+    <div id="fixed_block">
+
+    </div>
+  </div>
 
   <nav class="navbar">
     <a href="../Homepage/index.php"><img class="nav-brand" src="../img/icon(title).png" alt=""></a>
     <div class=" nav-list">
-      <div class = "Headee" id = "Homepage" ><a href="../Homepage/index.php"><b>首頁</b></a></div>
-      <div class = "Headee" id = "Popular" ><a href="#"><b>流行</b></a></div>
-      <div class = "Headee" id = "Recommendation"><a href="#"><b>推薦</b></a></div>
+      <div class="Headee" id="Homepage"><a href="../Homepage/index.php"><b>首頁</b></a></div>
+      <div class="Headee" id="Popular"><a href="#"><b>流行</b></a></div>
+      <div class="Headee" id="Recommendation"><a href="#"><b>推薦</b></a></div>
     </div>
     </div>
   </nav>
@@ -35,20 +37,20 @@
         <!--2--> <input type="password" id="password" class="password1" name="password" placeholder="密碼" required>
         <!--3--> <input type="button" value="登入" class="btn" id="submit" onclick="myFormCheck()" />
         <?php
+        include("../config/connect.php");
         $sql = "SELECT * FROM member";
         $res = mysqli_query($conn, $sql) or die(mysqli_error($link));
         $state = 0;
         while ($rows = mysqli_fetch_assoc($res)) {
-            $name = $rows['Name'];
-            $password = $rows['Password'];
-            if($name == $_GET['username'] and $password==$_GET['password']){
-              $state = 1;
-              break;
-            }
-            else $state =0;
-          }
-          if($state == 1)alert("successfully login");
-          else alert("failure");
+          $name = $rows['E-mail'];
+          $password = $rows['Password'];
+          if ($name == $_POST['username'] and $password == $_POST['password']) {
+            $state = 1;
+            break;
+          } else $state = 0;
+        }
+        if ($state == 1) echo "successfully login";
+        else echo "failure";
         ?>
         <div id="anotherWay">
           <div class="forgetPassword">
@@ -57,11 +59,12 @@
           <div id="sign">
             <a href="./Register/index.php" id="register">註冊</a>
           </div>
-                </fieldset>
-            </div><!--two-thirds-->
-        </form>
+      </fieldset>
+    </div>
+    <!--two-thirds-->
+  </form>
 
-    </body>
+</body>
 <style media="screen">
   @import url(Login.css);
 </style>
